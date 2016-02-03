9029f78c0798b5fab34310059cac1d0f92ea1361
Alexey Elagin <aelagin@mirantis.com>
2016-02-03 19:20:26 +0300

    it works!

--- etree-parser.py
+++ etree-parser.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 
-import xml.etree.ElementTree as etree
-
+from lxml import etree
 tree = etree.parse('document.xml')
-root = tree.getroot()
 
-print root
 
-print len(root)
-for child in root:
-    print '> ', child
-    print '> ', child.attrib
-    for next_child in child:
-        print '>> ', next_child
-        print '>> ', next_child.attrib
+nics = tree.xpath('/list/node/node[@id="core"]/descendant::node[@class="network"]')
+for nic in nics:
+    nic_name = nic.find('product')
+    nic_type = nic.find('description')
+    nic_vendor = nic.find('vendor')
+    nic_driver_name = nic.find('configuration/setting[@id="driver"]').get('value')
+    nic_driver_ver = nic.find('configuration/setting[@id="driverversion"]').get('value')
+    print nic_name.text
+    print nic_vendor.text
+    print nic_driver_name, nic_driver_ver
 
-all_ethernet = tree.findall('//class')
