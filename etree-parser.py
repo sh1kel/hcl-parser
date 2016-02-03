cc0835fb52765ebc17e381890f271c7e90355ad0
aelagin <aelagin@localhost.localdomain>
2016-02-03 17:28:28 +0300

    works!

--- etree-parser.py
+++ etree-parser.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python
 
-import xml.etree.ElementTree as etree
-
+xml = 'document.xml'
+from lxml import etree
 tree = etree.parse('document.xml')
-root = tree.getroot()
 
-print root
 
-print len(root)
-for child in root:
-    print '> ', child
-    print '> ', child.attrib
-    for next_child in child:
-        print '>> ', next_child
-        print '>> ', next_child.attrib
-
-all_ethernet = tree.findall('//class')
+nodes = tree.xpath('/list/node/node[@id="core"]/node[starts-with(@id,"pci:")]/node[@class="network"]/product') 
+#print nodes
+for node in nodes:
+	print node.text
+	#print node.tag
+	#print node.keys()
+	#print node.values()
