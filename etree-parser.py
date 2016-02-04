73617f68775638588dc7dc5fd641f151fbc4e8b5
Alexey Elagin <aelagin@mirantis.com>
2016-02-04 20:18:02 +0300

    works fine

--- etree-parser.py
+++ etree-parser.py
@@ -1,45 +1,90 @@
-#!/usr/bin/python
+#!/usr/local/bin/python2.7
 
 from lxml import etree
-tree = etree.parse('document2.xml')
+import sys
+import re
+import string
 
+xml_line = ''
+
+if len(sys.argv) < 2:
+    print "Use the Params Luke..."
+    print sys.argv[0], " hw_report_filename.txt"
+    exit(0)
+
+print "Parsing ", sys.argv[1]
+document = sys.argv[1]
+
+try:
+    xml_report = open(document, 'r')
+except IOError as e:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    exit(1)
+
+for line in xml_report:
+    if line.startswith('Date:'):
+        line.strip()
+        twodots = line.find(':')
+        v_date = line[twodots+1:].strip()
+        print v_date
+        continue
+    if line.startswith('Fuel version'):
+        line.strip()
+        twodots = line.find(':')
+        v_fuel =  line[twodots+1:].strip()
+        print v_fuel
+        continue
+    if line.startswith('<?xml'):
+        xml_line = line #[0:-1]
+        continue
+    if line.strip().startswith('<') and line.strip() != '</list>':
+        xml_line += line #[0:-1]
+    if line.strip() == '</list>':
+        xml_line += line #[0:-1]
+        break
+
+if len(xml_line.strip()) > 0:
+    tree = etree.fromstring(xml_line)
+else:
+    print "XML file is broken"
+    exit(0)
 
 nics = tree.xpath('/list/node/node[@id="core"]/descendant::node[@class="network"]')
 for nic in nics:
-    nic_name = nic.find('product')
-    nic_type = nic.find('description')
-    nic_vendor = nic.find('vendor')
-    nic_driver_name_obj = nic.find('configuration/setting[@id="driver"]')
-    nic_driver_ver_obj = nic.find('configuration/setting[@id="driverversion"]')
-    try:
-        nic_driver_name = nic_driver_name_obj.get('value')
-    except:
-        nic_driver_name = "unknown"
-    try:
-        nic_driver_ver = nic_driver_ver_obj.get('value')
-    except:
-        nic_driver_ver = "unknown"
-    print "Device: ", nic_name.text
-    print "Vendor: ", nic_vendor.text
-    print "Driver name: ", nic_driver_name
-    print "Driver version: ", nic_driver_ver
+        nic_name = nic.find('product')
+        nic_type = nic.find('description')
+        nic_vendor = nic.find('vendor')
+        nic_driver_name_obj = nic.find('configuration/setting[@id="driver"]')
+        nic_driver_ver_obj = nic.find('configuration/setting[@id="driverversion"]')
+        try:
+                nic_driver_name = nic_driver_name_obj.get('value')
+        except:
+                nic_driver_name = "unknown"
+        try:
+                nic_driver_ver = nic_driver_ver_obj.get('value')
+        except:
+                nic_driver_ver = "unknown"
+        print "Device: ", nic_name.text
+        print "Vendor: ", nic_vendor.text
+        print "Driver name: ", nic_driver_name
+        print "Driver version: ", nic_driver_ver
 
 raids = tree.xpath('/list/node/node[@id="core"]/descendant::node[starts-with(@id,"storage") and @class="storage"]')
 for raidcnt in raids:
-    raid_name = raidcnt.find('product')
-    raid_type = raidcnt.find('description')
-    raid_vendor = raidcnt.find('vendor')
-    raid_driver_name_obj = raidcnt.find('configuration/setting[@id="driver"]')
-    raid_driver_ver_obj = raidcnt.find('configuration/setting[@id="driverversion"]')
-    try:
-        raid_driver_name = raid_driver_name_obj.get('value')
-    except:
-        raid_driver_name = "unknown"
-    try:
-        raid_driver_ver = raid_driver_ver_obj.get('value')
-    except:
-        raid_driver_ver = "unknown"
-    print "Device: ", raid_name.text
-    print "Vendor: ", raid_vendor.text
-    print "Driver name: ", raid_driver_name
-    print "Driver version: ", raid_driver_ver
+        raid_name = raidcnt.find('product')
+        raid_type = raidcnt.find('description')
+        raid_vendor = raidcnt.find('vendor')
+        raid_driver_name_obj = raidcnt.find('configuration/setting[@id="driver"]')
+        raid_driver_ver_obj = raidcnt.find('configuration/setting[@id="driverversion"]')
+        try:
+                raid_driver_name = raid_driver_name_obj.get('value')
+        except:
+                raid_driver_name = "unknown"
+        try:
+                raid_driver_ver = raid_driver_ver_obj.get('value')
+        except:
+                raid_driver_ver = "unknown"
+        print "Device: ", raid_name.text
+        print "Vendor: ", raid_vendor.text
+        print "Driver name: ", raid_driver_name
+        print "Driver version: ", raid_driver_ver
