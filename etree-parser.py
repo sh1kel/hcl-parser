c7e8fcc071a07f4019156a9155d4fe2a19a80d20
aelagin <aelagin@localhost.localdomain>
2016-02-05 19:54:17 +0300

    changes!

--- etree-parser.py
+++ etree-parser.py
@@ -1,14 +1,155 @@
-#!/usr/bin/python
+#!/usr/local/bin/python2.7
 
-xml = 'document.xml'
 from lxml import etree
-tree = etree.parse('document.xml')
+import sys
+from sqlalchemy import create_engine, Table, MetaData, orm
 
+# db credentials
+db_user = 'root'
+db_pass = 'Admin112'
+db_host = 'localhost'
+db_name = 'hcl'
 
-nodes = tree.xpath('/list/node/node[@id="core"]/node[starts-with(@id,"pci:")]/node[@class="network"]/product') 
-#print nodes
-for node in nodes:
-	print node.text
-	#print node.tag
-	#print node.keys()
-	#print node.values()
+xml_line = ''
+sql_engine = create_engine("mysql://db_user:db_pass@db_host/db_name", echo=True)
+
+# DB prepare
+Base = declarative_base()
+
+class validation(Base):
+    __tablename__ = 'validation'
+    id                  = Column(integer, primary_key=True)
+    server_id           = Column(integer)
+    release_id          = Column(integer)
+    val_date            = Column(datetime)
+    customized_bootstrap = Column(integer)
+    notes               = Column(string)
+
+class server(Base):
+    __tablename__       = 'server'
+    id                  = Column(integer, primary_key=True)
+    server_vendor_id    = Column(integer)
+    name                = Column(string)
+    notes               = Column(string)
+
+class server_vendor(Base):
+    __tablename__       = 'server_vendor'
+    id                  = Column(integer, primary_key=True)
+    name                = Column(string)
+
+class releases(Base):
+    __tablename__       = 'releases'
+    id                  = Column(integer, primary_key=True)
+    name                = Column(string)
+
+class dev_to_validation(Base):
+    __tablename__       = 'dev-to-validation'
+    id                  = Column(integer, primary_key=True)
+    validation_id       = Column(integer)
+    device_id           = Column(integer)
+    driver_name         = Column(string)
+    driver_ver          = Column(string)
+    is_work             = Column(bool)
+
+class device(Base):
+    __tablename__       = 'device'
+    id                  = Column(integer, primary_key=True)
+    name                = Column(string)
+    type                = Column(string)
+    description         = Column(string)
+    device_maker_id     = Column(integer)
+
+class device_maker(Base):
+    __tablename__       = 'device_maker'
+    id                  = Column(integer, primary_key=True)
+    name                = Column(string)
+
+# check for argument - file to parse
+if len(sys.argv) < 2:
+    print "Use the Params Luke..."
+    print sys.argv[0], " hw_report_filename.txt"
+    exit(0)
+
+print "Parsing ", sys.argv[1]
+document = sys.argv[1]
+
+# try to open report file
+try:
+    xml_report = open(document, 'r')
+except IOError as e:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    exit(1)
+
+# parsing
+for line in xml_report:
+    if line.startswith('Date:'):            # Date field
+        line.strip()
+        twodots = line.find(':')            # search for :
+        v_date = line[twodots+1:].strip()   # get value after :
+        print v_date
+        continue
+    if line.startswith('Fuel version'):
+        line.strip()
+        twodots = line.find(':')
+        v_fuel =  line[twodots+1:].strip()
+        print v_fuel
+        continue
+    if line.startswith('<?xml'):            # search for starting of xml
+        xml_line = line
+        continue
+    if line.strip().startswith('<') and line.strip() != '</list>':      # xml
+        xml_line += line                    # add xml lines to str
+    if line.strip() == '</list>':           # xml ends
+        xml_line += line
+        break
+
+if len(xml_line.strip()) > 0:               # if xml string size not null
+    tree = etree.fromstring(xml_line)       # parse xml
+else:
+    print "XML file is broken"
+    exit(0)
+
+# NICs
+# looking for nodes with class network
+nics = tree.xpath('/list/node/node[@id="core"]/descendant::node[@class="network"]') 
+for nic in nics:
+	nic_name = nic.find('product')
+	nic_type = nic.find('description')
+	nic_vendor = nic.find('vendor')
+    # lookging for subvalues with drivers info
+	nic_driver_name_obj = nic.find('configuration/setting[@id="driver"]')
+	nic_driver_ver_obj = nic.find('configuration/setting[@id="driverversion"]')
+	try:
+		nic_driver_name = nic_driver_name_obj.get('value')
+	except:
+		nic_driver_name = "unknown"
+	try:
+		nic_driver_ver = nic_driver_ver_obj.get('value')
+	except:
+		nic_driver_ver = "unknown"
+	print "Device: ", nic_name.text
+	print "Vendor: ", nic_vendor.text
+	print "Driver name: ", nic_driver_name
+	print "Driver version: ", nic_driver_ver
+
+# RAIDs
+# looking for nodes which ids starts from storage and has class storage
+raids = tree.xpath('/list/node/node[@id="core"]/descendant::node[starts-with(@id,"storage") and @class="storage"]') 
+for raidcnt in raids:
+	raid_name = raidcnt.find('product')
+	raid_type = raidcnt.find('description')
+	raid_vendor = raidcnt.find('vendor')
+	raid_driver_name_obj = raidcnt.find('configuration/setting[@id="driver"]')
+	raid_driver_ver_obj = raidcnt.find('configuration/setting[@id="driverversion"]')
+	try:
+		raid_driver_name = raid_driver_name_obj.get('value')
+	except:
+		raid_driver_name = "unknown"
+	try:	
+		raid_driver_ver = raid_driver_ver_obj.get('value')
+	except:
+		raid_driver_ver = "unknown"
+	print "Device: ", raid_name.text
+	print "Vendor: ", raid_vendor.text
+	print "Driver name: ", raid_driver_name
+	print "Driver version: ", raid_driver_ver
