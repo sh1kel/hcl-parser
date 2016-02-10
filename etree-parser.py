dcc7f45a6806c64bac3e1439c8373cfeaaca6ca8
Alexey Elagin <aelagin@mirantis.com>
2016-02-10 16:45:20 +0300

    change table description

--- etree-parser.py
+++ etree-parser.py
@@ -1,8 +1,9 @@
 #!/usr/local/bin/python2.7
 
-from lxml import etree
-import sys
-from sqlalchemy import create_engine, Table, MetaData, orm
+from sqlalchemy import create_engine, Table, MetaData, orm, Integer, ForeignKey, Column, String, DateTime, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
 
 # db credentials
 db_user = 'root'
@@ -17,52 +18,53 @@ sql_engine = create_engine("mysql://db_user:db_pass@db_host/db_name", echo=True)
 Base = declarative_base()
 
 class validation(Base):
-    __tablename__ = 'validation'
-    id                  = Column(integer, primary_key=True, autoincrement=True)
-    server_id           = Column(integer, nullable=False)
-    release_id          = Column(integer, nullable=False))
+    __tablename__       = 'validation'
+    id                  = Column(Integer, primary_key=True, autoincrement=True)
+    server_id           = Column(Integer, nullable=False)
+    release_id          = Column(Integer, nullable=False)
     val_date            = Column(DateTime)
-    customized_bootstrap = Column(integer)
-    notes               = Column(string)
+    customized_bootstrap = Column(Integer)
+    notes               = Column(String)
 
 class server(Base):
     __tablename__       = 'server'
-    id                  = Column(integer, primary_key=True, ForeignKey("validation.server_id"), nullable=False, autoincrement=True)
-    server_vendor_id    = Column(integer, nullable=False)
-    name                = Column(string(128), unique=True)
-    notes               = Column(string)
+    id                  = Column(Integer, ForeignKey("validation.server_id"), primary_key=True, nullable=False, autoincrement=True)
+    server_vendor_id    = Column(Integer, nullable=False)
+    name                = Column(String(128), unique=True)
+    notes               = Column(String)
 
 class server_vendor(Base):
     __tablename__       = 'server_vendor'
-    id                  = Column(integer, primary_key=True, ForeignKey("server.server_verndor_id"), nullable=False, autoincrement=True)
-    name                = Column(string(128), unique=True)
+    id                  = Column(Integer, ForeignKey("server.server_verndor_id"), primary_key=True, nullable=False, autoincrement=True)
+    name                = Column(String(128), unique=True)
 
 class releases(Base):
     __tablename__       = 'releases'
-    id                  = Column(integer, primary_key=True, ForeignKey("validation.release_id"), nullable=False, autoincrement=True)
-    name                = Column(string(64))
+    id                  = Column(Integer, ForeignKey("validation.release_id"), primary_key=True, nullable=False, autoincrement=True)
+    name                = Column(String(64))
 
 class dev_to_validation(Base):
     __tablename__       = 'dev-to-validation'
-    id                  = Column(integer, primary_key=True, autoincrement=True)
-    validation_id       = Column(integer, ForeignKey("validation.id"), nullable=False)
-    device_id           = Column(integer)
-    driver_name         = Column(string(128))
-    driver_ver          = Column(string(64))
-    is_work             = Column(bool)
+    id                  = Column(Integer, primary_key=True, autoincrement=True)
+    validation_id       = Column(Integer, ForeignKey("validation.id"), nullable=False)
+    device_id           = Column(Integer)
+    driver_name         = Column(String(128))
+    driver_ver          = Column(String(64))
+    is_work             = Column(Boolean)
 
 class device(Base):
     __tablename__       = 'device'
-    id                  = Column(integer, primary_key=True, autoincrement=True)
-    name                = Column(string(128), unique=True, ForeignKey("dev-to-validation.device_id"), nullable=False)
-    type                = Column(string(64))
-    description         = Column(string)
-    device_maker_id     = Column(integer)
+    id                  = Column(Integer, primary_key=True, autoincrement=True)
+    name                = Column(String(128), ForeignKey("dev-to-validation.device_id"), unique=True, nullable=False)
+    type                = Column(String(64))
+    description         = Column(String)
+    device_maker_id     = Column(Integer)
 
 class device_maker(Base):
     __tablename__       = 'device_maker'
-    id                  = Column(integer, primary_key=True, ForeignKey("device.device_maker_id"), nullable=False, autoincrement=True)
-    name                = Column(string(128))
+    id                  = Column(Integer, ForeignKey("device.device_maker_id"), primary_key=True, nullable=False, autoincrement=True)
+    name                = Column(String(128))
+
 
 
 
