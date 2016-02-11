212fc3f261c2ff25021e9e57afc78a9ca31e375e
sh1kel <me@sh1kel.com>
2016-02-11 16:06:08 +0300

    add some logic

--- etree-parser.py
+++ etree-parser.py
@@ -1,22 +1,20 @@
-#!/usr/local/bin/python2.7
-
+from lxml import etree                                                                                                                                                              [200/1926]
+import sys
 from sqlalchemy import create_engine, Table, MetaData, orm, Integer, ForeignKey, Column, String, DateTime, Boolean
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 
 # db credentials
 db_user = 'root'
-db_pass = ''
+db_pass = 'Admin112'
 db_host = 'localhost'
-db_name = 'hcl'
+db_name = 'hcl_test'
 
 xml_line = ''
-sql_engine = create_engine("mysql://" + db_user + ":" + db_pass + "@" + db_host +"/" + db_name, echo=True)
+sql_engine = create_engine("mysql://" + db_user + ":" + db_pass + "@" + db_host +"/" + db_name) #, echo=True)
 Session = sessionmaker(bind=sql_engine)
 session = Session()
-
-
 # DB prepare
 Base = declarative_base()
 
@@ -39,7 +37,8 @@ class Server_vendor(Base):
     id                  = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name                = Column(String(128), unique=True, nullable=False)
     server              = relationship("Server", backref=backref('servers', uselist=True, cascade='delete,all'))
-    
+
+
 class Server(Base):
     __tablename__       = 'server'
     __table_args__      = {'mysql_engine':'InnoDB'}
@@ -49,15 +48,16 @@ class Server(Base):
     notes               = Column(String(255))
     vendor              = relationship("Server_vendor")
     validation          = relationship("Validation")
-    
+
+
 class Releases(Base):
     __tablename__       = 'releases'
     __table_args__      = {'mysql_engine':'InnoDB'}
     id                  = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name                = Column(String(64), unique=True, nullable=False)
     validation          = relationship("Validation", backref=backref('validation'), uselist=True, cascade='delete,all')
-    
-class Dev_to_validation(Base):
+
+class Dev_to_validation(Base):                                                                                                                                                      [141/1926]
     __tablename__       = 'dev_to_validation'
     __table_args__      = {'mysql_engine':'InnoDB'}
     id                  = Column(Integer, primary_key=True, autoincrement=True)
@@ -68,7 +68,8 @@ class Dev_to_validation(Base):
     is_work             = Column(Boolean)
     validation          = relationship("Validation")
     device              = relationship("Device")
-    
+
+
 class Device(Base):
     __tablename__       = 'device'
     __table_args__      = {'mysql_engine':'InnoDB'}
@@ -79,7 +80,7 @@ class Device(Base):
     device_maker_id     = Column(Integer, ForeignKey("device_maker.id"), primary_key=True, nullable=False)
     maker               = relationship("Device_maker")
     dtv                 = relationship("Dev_to_validation")
-    
+
 class Device_maker(Base):
     __tablename__       = 'device_maker'
     __table_args__      = {'mysql_engine':'InnoDB'}
@@ -87,8 +88,26 @@ class Device_maker(Base):
     name                = Column(String(128), unique=True, nullable=False)
     device              = relationship("Device", backref=backref('devices', uselist=True, cascade='delete,all'))
 
-Base.metadata.create_all(sql_engine)
+#Base.metadata.create_all(sql_engine)
 
+"""
+for val in session.query(device.name,\
+    device_maker.name,\
+    dev_to_validation.driver_name,\
+    dev_to_validation.driver_ver,\
+    server.name,\
+    server_vendor.name,\
+    releases.name).\
+    filter(validation.server_id == server.id).\
+    filter(validation.release_id == releases.id).\
+    filter(server.server_vendor_id == server_vendor.id).\
+    filter(dev_to_validation.validation_id == validation.id).\
+    filter(device.id == dev_to_validation.device_id).\
+    filter(device.device_maker_id == device_maker.id):
+    print val
+"""
+
+# check for argument - file to parse
 if len(sys.argv) < 2:
     print "Use the Params Luke..."
     print sys.argv[0], " hw_report_filename.txt"
@@ -97,41 +116,42 @@ if len(sys.argv) < 2:
 print "Parsing ", sys.argv[1]
 document = sys.argv[1]
 
+# try to open report file                                                                                                                                                            [82/1926]
 try:
     xml_report = open(document, 'r')
 except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)
     exit(1)
 
+# parsing
 for line in xml_report:
-    if line.startswith('Date:'):
+    if line.startswith('Date:'):            # Date field
         line.strip()
-        twodots = line.find(':')
-        v_date = line[twodots+1:].strip()
-        print v_date
+        twodots = line.find(':')            # search for :
+        v_date = line[twodots+1:].strip()   # get value after :
+        #print v_date
         continue
     if line.startswith('Fuel version'):
         line.strip()
         twodots = line.find(':')
         v_fuel =  line[twodots+1:].strip()
-        print v_fuel
+        #print v_fuel
         continue
-    if line.startswith('<?xml'):
-        xml_line = line #[0:-1]
+    if line.startswith('<?xml'):            # search for starting of xml
+        xml_line = line
         continue
-    if line.strip().startswith('<') and line.strip() != '</list>':
-        xml_line += line #[0:-1]
-    if line.strip() == '</list>':
-        xml_line += line #[0:-1]
+    if line.strip().startswith('<') and line.strip() != '</list>':      # xml
+        xml_line += line                    # add xml lines to str
+    if line.strip() == '</list>':           # xml ends
+        xml_line += line
         break
 
-if len(xml_line.strip()) > 0:
-    tree = etree.fromstring(xml_line)
+if len(xml_line.strip()) > 0:               # if xml String size not null
+    tree = etree.fromstring(xml_line)       # parse xml
 else:
     print "XML file is broken"
     exit(0)
 
-    
 # get server info
 server_info = tree.xpath('/list/node')
 # server name
@@ -144,54 +164,96 @@ if server_name.find('To be filled by') != -1:
     server_name = server_name.split('(')[0]
     server_name = server_name.rstrip()
 
+#validation_obj = validation                                                                                                                                                         [34/1926]
+# NICs
+# looking for nodes with class network
+step = 1
+nics = tree.xpath('/list/node/node[@id="core"]/descendant::node[@class="network"]')
+for nic in nics:
+    print "Device #", step, "found!"
+    nic_name = nic.find('product')
+    nic_type = nic.find('description')
+    nic_vendor = nic.find('vendor')
+    # lookging for subvalues with drivers info
+    nic_driver_name_obj = nic.find('configuration/setting[@id="driver"]')
+    nic_driver_ver_obj = nic.find('configuration/setting[@id="driverversion"]')
+    try:
+        nic_driver_name = nic_driver_name_obj.get('value')
+    except:
+        nic_driver_name = "unknown"
+    try:
+        nic_driver_ver = nic_driver_ver_obj.get('value')
+    except:
+        nic_driver_ver = "unknown"
+    try:
+        # check if found device is in DB
+        dev = session.query(Device).filter(Device.name == nic_name.text).one()
+        print "Device", dev.name, "already in DB"
+    # if there is no that device in DB...
+    except:
+        try:
+            # check if device's vendor is in DB
+            devmaker = session.query(Device_maker).filter(Device_maker.name == nic_vendor.text).one()
+            # if vendor is in DB...
+            device_obj = Device(name = nic_name.text, type = 'nic', device_maker_id = devmaker.id)
+            session.add(device_obj)
+            print "Vendor", devmaker.name, "already in DB"
+            print "Adding device", device_obj.name, " to DB"
+        except:
+            # if vendor is not in DB...
+            print "Vendor", nic_vendor.text, "is not in DB..."
+            device_maker_obj = Device_maker(name = nic_vendor.text)
+            device_obj = Device(name = nic_name.text, type = 'nic', maker = device_maker_obj)
+            session.add(device_maker_obj)
+            session.add(device_obj)
+            print "Adding vendor", device_maker_obj.name, "to DB"
+            print "Adding device", device_obj.name, " to DB"
+    session.commit()
+    step = step+1
+    #print maker_exist
+    #print "Device: ", nic_name.text
+    #print "Vendor: ", nic_vendor.text
+    #print "Driver name: ", nic_driver_name
+    #print "Driver version: ", nic_driver_ver
+    #device_maker_obj = Device_maker(name = nic_vendor.text)
+    #device_obj = Device(name = nic_name.text, type = 'nic', maker = device_maker_obj)
+    #session.add(device_maker_obj)
+    #session.add(device_obj)
+    #session.commit()
+
+# RAIDs
+# looking for nodes which ids starts from storage and has class storage
+raids = tree.xpath('/list/node/node[@id="core"]/descendant::node[starts-with(@id,"storage") and @class="storage"]')
+for raidcnt in raids:
+    raid_name = raidcnt.find('product')
+    raid_type = raidcnt.find('description')
+    raid_vendor = raidcnt.find('vendor')
+    raid_driver_name_obj = raidcnt.find('configuration/setting[@id="driver"]')
+    raid_driver_ver_obj = raidcnt.find('configuration/setting[@id="driverversion"]')
+    try:
+        raid_driver_name = raid_driver_name_obj.get('value')
+    except:
+        raid_driver_name = "unknown"
+    try:
+        raid_driver_ver = raid_driver_ver_obj.get('value')
+    except:
+        raid_driver_ver = "unknown"
+    #print "Device: ", raid_name.text
+    #print "Vendor: ", raid_vendor.text
+    #print "Driver name: ", raid_driver_name
+    #print "Driver version: ", raid_driver_ver
+    #device_maker_obj = Device_maker(name = nic_vendor.text)
+    #device_obj = Device(name = nic_name.text, type = 'raid', maker = device_maker_obj)
+    #session.add(device_maker_obj)
+    #session.add(device_obj)
+    #session.commit()
+
 # vendor sql object
 server_vendor_obj = Server_vendor(name=server_vendor_name)
 # server sql object
 server_obj = Server(name = server_name, vendor = server_vendor_obj)
+release_obj = Releases(name = v_fuel)
 
-session.add(server_vendor_obj)
-session.add(server_obj)
-session.commit()
-
-
-
-
-nics = tree.xpath('/list/node/node[@id="core"]/descendant::node[@class="network"]')
-for nic in nics:
-        nic_name = nic.find('product')
-        nic_type = nic.find('description')
-        nic_vendor = nic.find('vendor')
-        nic_driver_name_obj = nic.find('configuration/setting[@id="driver"]')
-        nic_driver_ver_obj = nic.find('configuration/setting[@id="driverversion"]')
-        try:
-                nic_driver_name = nic_driver_name_obj.get('value')
-        except:
-                nic_driver_name = "unknown"
-        try:
-                nic_driver_ver = nic_driver_ver_obj.get('value')
-        except:
-                nic_driver_ver = "unknown"
-        print "Device: ", nic_name.text
-        print "Vendor: ", nic_vendor.text
-        print "Driver name: ", nic_driver_name
-        print "Driver version: ", nic_driver_ver
-
-raids = tree.xpath('/list/node/node[@id="core"]/descendant::node[starts-with(@id,"storage") and @class="storage"]')
-for raidcnt in raids:
-        raid_name = raidcnt.find('product')
-        raid_type = raidcnt.find('description')
-        raid_vendor = raidcnt.find('vendor')
-        raid_driver_name_obj = raidcnt.find('configuration/setting[@id="driver"]')
-        raid_driver_ver_obj = raidcnt.find('configuration/setting[@id="driverversion"]')
-        try:
-                raid_driver_name = raid_driver_name_obj.get('value')
-        except:
-                raid_driver_name = "unknown"
-        try:
-                raid_driver_ver = raid_driver_ver_obj.get('value')
-        except:
-                raid_driver_ver = "unknown"
-        print "Device: ", raid_name.text
-        print "Vendor: ", raid_vendor.text
-        print "Driver name: ", raid_driver_name
-        print "Driver version: ", raid_driver_ver
+#session.add(server_vendor_obj)
+#session.add(server_obj)
+#session.commit()
